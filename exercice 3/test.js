388224c6ac94e9ba7f6b53281407797bd45151a1
Gael Touquet <gael.touquet@gmail.com>
2022-11-21 16:41:12 +0100

    bugfix

--- exercice 3/test.js
+++
@@ -1,3 +0,0 @@
-import { Personnage, Aventurier, Paladin, Guerrier, Mage } from "./Personnage.js";
-
-myperso = new Guerrier("Conan");
---
+++ exercice 3/test.mjs
@@ -0,0 +1,3 @@
+import { Personnage, Aventurier, Paladin, Guerrier, Mage } from "./Personnage.mjs";
+
+let myperso = new Guerrier("Conan");
